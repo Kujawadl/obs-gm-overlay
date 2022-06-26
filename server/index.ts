47dfbce7b56dc490d9127e320a889c8635352dcf
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2022-06-26 14:59:32 -0500

    Switch to apollo-server-express

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,34 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import express from "express";
+import http from "http";
+import { AddressInfo } from "net";
 
 import typeDefs from "./types";
 import resolvers from "./resolvers";
 import { setupContext } from "./context";
 
-async function setupServer() {
+async function startApolloServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
   const context = await setupContext();
-  return new ApolloServer({
+  const server = new ApolloServer({
     typeDefs,
     resolvers,
     csrfPrevention: true,
     cache: "bounded",
     context,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
+
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: "/",
+  });
+
+  await httpServer.listen({ port: 4000 });
+  console.log(`🚀  Server ready at port ${4000}`);
 }
 
-setupServer().then((server) =>
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-  })
-);
+startApolloServer();
--- server/types/index.ts
+++ server/types/index.ts
@@ -1,4 +1,4 @@
-import { gql } from "apollo-server";
+import { gql } from "apollo-server-core";
 import Campaign from "./campaign";
 import Player from "./player";
 
