1673d2a2b97cd467b59b8bdb00d491721e831ae3
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2022-07-13 21:47:02 -0500

    Fix migration script

--- migrate.ts
+++ migrate.ts
@@ -1,7 +1,6 @@
 import { join } from "path";
 import sqlite from "better-sqlite3-helper";
 
-// The first call creates the global instance with your settings
 sqlite({
 	path: join(process.cwd(), "obs-gm-overlay.db"),
 	readonly: false,
@@ -11,4 +10,4 @@ sqlite({
 		table: "migrations",
 		migrationsPath: "./migrations",
 	},
-});
+}).migrate();
